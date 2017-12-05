411363a2c52c2f671a6c78c006b6eba67366cc1c
Racheal <32204500+racheallarimer@users.noreply.github.com>
2017-12-04 21:10:25 -0800

    Update christmasPicker.js

--- christmasPicker.js
+++ christmasPicker.js
@@ -1,4 +1,4 @@
-const christmasMovies = ['Elf', 'Home Alone', 'A Christmas Story', 'Love Actually', 'Christmas Vacation', 'The Santa Clause', 'Scrooged', 'Mixed Nuts', 'The Holiday', 'Bad Santa', 'Gremlins', 'Gremlins 2', 'Christmas in Connecticut', 'Holiday Inn', "I'll Be Home for Christmas", 'Trading Places', 'White Christmas', 'Die Hard', 'How the Grinch Stole Christmas', 'The Family Man', 'Just Friends', 'Kiss Kiss, Bang Bang', 'The Night Before', 'Krampus', 'Jingle All the Way', 'Carol', 'Tangerine', 'Arthur Christmas'];
+const christmasMovies = ['Elf', 'Home Alone', 'A Christmas Story', 'Love Actually', 'Christmas Vacation', 'The Santa Clause', 'Scrooged', 'Mixed Nuts', 'The Holiday', 'Bad Santa', 'Gremlins', 'Christmas in Connecticut', 'Holiday Inn', "I'll Be Home for Christmas", 'White Christmas', 'Die Hard', 'How the Grinch Stole Christmas', 'The Family Man', 'Just Friends', 'Kiss Kiss, Bang Bang', 'The Night Before', 'Jingle All the Way', 'Arthur Christmas'];
 
 const pickMovie = () => {
 	let movieChoice = christmasMovies[Math.floor(Math.random() * (christmasMovies.length - 1))];
